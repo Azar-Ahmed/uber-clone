3d3935e170bf4d3ff1535f07e38f34c44a2f2e91
ow1us0jp <qadriiazhar@gmail.com>
2025-03-10 13:38:40 +0530

    inital commit setup backend server

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import express from 'express'
+import cors from 'cors'
+
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+
+
+app.get('/', (req, res)=>{
+    res.send(`Server is up!`);
+})
+
+export default app;
